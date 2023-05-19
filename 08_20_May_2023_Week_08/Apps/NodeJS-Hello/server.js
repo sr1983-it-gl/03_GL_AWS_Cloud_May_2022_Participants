e93a89401227b998621d173fe34eea5ca311765b
sr1983-it-gl <sr1983.it.gl@gmail.com>
2023-05-20 02:24:17 +0530

    Files

---
+++ 08_20_May_2023_Week_08/Apps/NodeJS-Hello/server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+require("dotenv").config();
+const path = require('path');
+
+// Constants
+const PORT = process.env.PORT || 8080;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+
+  res.sendFile(path.join(__dirname, '/index.html'));  
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
